b2e0cf86038180c683ceebdd51346959e1c63c47
yoshizawa-code <73457180+yoshizawa-code@users.noreply.github.com>
2023-03-16 16:19:21 +0900

    Update api_list.py

--- api_list.py
+++ api_list.py
@@ -1,2 +1,2 @@
 DEV_API_LIST = ["http://127.0.0.1:8001","http://127.0.0.1:8003"]
-PRD_API_LIST = ["https://render-wazt.onrender.com/", "https://render3-qru6.onrender.com/"]
+PRD_API_LIST = ["https://render-wazt.onrender.com", "https://render3-qru6.onrender.com"]
